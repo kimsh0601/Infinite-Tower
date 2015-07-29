859ea7eabe4a1f33a45e45636cd1ff00f327f60f
kimsh0601 <dlclsh321@naver.com>
2015-07-29 12:58:31 +0900

    GamePlay_1

---
+++ Assets/Scripts/Monster.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Monster : MonoBehaviour {
+    public Transform target;
+    public Vector3 direction;
+
+    public Vector3 AutoMove;
+    public float Auto_vel;
+
+    public float velocity;
+    public float M_spd;
+    public float MTag;
+
+
+	// Use this for initialization
+	void Start () {
+
+        RandomMove();
+        Auto_vel = 0.01f;
+
+
+    }
+    void RandomMove() {
+        AutoMove.x = Random.Range(-1.0f, 1.0f);
+        AutoMove.y = Random.Range(-1.0f, 1.0f);
+        Invoke("RandomMove", 1.5f);
+    }
+
+	
+	// Update is called once per frame
+	void Update () {
+
+        target = GameObject.Find("PlayerEX").transform;
+        float distance = Vector3.Distance(target.position, transform.position);
+        if (distance <= MTag)
+        {
+            //CancelInvoke();
+            MoveTarget();
+            AutoMove = direction;
+        }
+        else
+        {
+            velocity = 0.0f;
+            this.transform.position = new Vector3(transform.position.x + (AutoMove.x * Auto_vel),
+                                                   transform.position.y + (AutoMove.y * Auto_vel),
+                                                   transform.position.z);
+        }
+
+
+    }
+   
+    void MoveTarget()
+    {       
+        //플레이어의 위치와 몬스터의 위치를 빼고 단위 백터화
+        direction = (target.position - transform.position).normalized;
+        
+        //초가 아닌 한 프레임으로 속도증가
+        velocity = (M_spd * Time.deltaTime);
+               
+        // 해당 방향으로 무우-빙
+        this.transform.position = new Vector3(transform.position.x + (direction.x * velocity),
+                                                   transform.position.y + (direction.y * velocity),
+                                                   transform.position.z);
+        
+      
+    }
+   
+
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,19 +1,58 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
 
-public class Player : MonoBehaviour {
+    public class Player : MonoBehaviour {
 
+    bool facingRight = true; // 캐릭터의 방향 전환을 위한 불값
+    Vector3 movement; // 상하좌우 움직임을 위한 벡터
+
+    public float P_mspd; // 플레이어 스피드
+
+    Animator anim;
+    Animation anime;
 	// Use this for initialization
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+        anime = GetComponent<Animation>();
+    }
 	void Start () {
-	
+        
+	    
 	}
-	
+    void Flip(){
+        facingRight = !facingRight;
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
+    }
 	// Update is called once per frame
 	void Update () {
 
-        float xx = Input.GetAxis("Horizontal");
-
-        transform.Translate(Vector3.right);
+        
 
 	}
+    void FixedUpdate()
+    {
+        Move();
+
+    }
+    void Move()
+    {
+        float h = CrossPlatformInputManager.GetAxisRaw("Horizontal");
+        float v = CrossPlatformInputManager.GetAxisRaw("Vertical");
+        movement.Set(h, v, 0);
+        if (h > 0 && !facingRight || h < 0 && facingRight) Flip();
+        if (h > 0 || h < 0 || v < 0 || v > 0) {
+            anim.SetBool("IsWalk", true);
+        }
+        else {
+            anim.SetBool("IsWalk", false);
+           
+           
+        }
+        transform.Translate(movement * P_mspd * Time.deltaTime);
+
+    }
 }
