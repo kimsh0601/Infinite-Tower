bb40f3ab0c741ac9ed6a63db196bbce8c7f7e9cb
Marogani <rladnwnd4968@naver.com>
2015-08-02 23:40:14 +0900

    Menu_1
    Menu change1

---
+++ Assets/Scripts/BtnManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BtnManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void GamePlay(){
+		Application.LoadLevel("GamePlay");
+	}
+}
