6fd2d872ecf9ea8cc6b4e9f9dcb64d109eb6030a
Marogani <rladnwnd4968@naver.com>
2015-08-05 03:23:17 +0900

    HIGI CARD_2
    OH HIHI CARD

---
+++ Assets/Scripts/ManagerScripts/FloorSelManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FloorSelManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	public void Town(){
+		Application.LoadLevel("Town");
+	}
+	public void FirstFloor(){
+		Application.LoadLevel("GamePlay");
+	}
+}
