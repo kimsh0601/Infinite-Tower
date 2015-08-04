7990be34936b9355c8e8b14e9122f44ff46a7716
Marogani <rladnwnd4968@naver.com>
2015-08-04 17:20:16 +0900

    Higi Card
    HIHI CARD

---
+++ Assets/Scripts/TownManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TownManager : MonoBehaviour {
+
+	private Collider2D Forge;
+	private Collider2D Tower;
+	// Use this for initialization
+	void Awake(){
+		Forge = GameObject.Find ("Forge").GetComponent<Collider2D> ();
+		Tower = GameObject.Find ("Tower").GetComponent<Collider2D> ();
+	}
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Forge.transform.name == "PlayerEX") {
+			Application.LoadLevel("Forge");
+		}
+	}
+	void OnTriggerEnter2D(Collider2D col){
+		if (Forge.transform.name == "PlayerEX") {
+			Application.LoadLevel("Forge");
+		}
+		if (Tower.transform.name == "PlayerEX") {
+			Application.LoadLevel("Tower");
+		}
+	}
+}
