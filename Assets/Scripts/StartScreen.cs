4ba393963572ffccb1f494394a63c3dcf68a6ef2
kimsh0601 <dlclsh321@naver.com>
2015-08-08 03:48:00 +0900

    commithun
    asdfsdfsdfsdf

--- Assets/Scripts/StartScreen.cs
+++ Assets/Scripts/StartScreen.cs
@@ -3,7 +3,13 @@ using System.Collections;
 
 public class StartScreen : MonoBehaviour {
 
-	// Use this for initialization
+    private int Tutorial;
+
+    // Use this for initialization
+    void Awake () {
+        Tutorial = PlayerPrefs.GetInt("char");
+    }
+
 	void Start () {
 	
 	}
@@ -11,7 +17,8 @@ public class StartScreen : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetMouseButtonDown(0)) {
-			Application.LoadLevel("Town");
+            if (Tutorial == 0) Application.LoadLevel("CharacterSelect");
+            else Application.LoadLevel("Town");
 		}
 	}
 }
